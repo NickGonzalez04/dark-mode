f3fe028e3ed5a44694c4482de672800440bc5804
Nick Gonzalez <gonzaleznick5@gmail.com>
2019-09-05 14:10:11 -0400

    MVP

---
+++ src/hooks/useDarkMode.jsx
@@ -0,0 +1,14 @@
+import { useEffect } from 'react'
+import useLocalStorage from './useLocalStorage'
+
+const useDarkMode = initialValue => {
+    const [displayMode, setDisplayMode] = useLocalStorage('Dark Mode', initialValue)
+
+    useEffect(() => {
+        displayMode ? document.body.classList.add('dark-mode') : document.body.classList.remove('dark-mode')        
+    }, [displayMode])
+
+    return [displayMode, setDisplayMode]
+}
+
+export default useDarkMode;
